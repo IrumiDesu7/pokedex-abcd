0135587ff3b6842fbff1aa8a763cbf081416bd99
IrumiDesu7 <ikalam89@gmail.com>
2026-02-25 15:20:27 +0800

    feat: add Zard shared UI component library (badge, button, card, dialog, icon, pagination, skeleton)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideZard } from '@/shared/core/provider/providezard';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    
-  ]
+    provideHttpClient(),
+    provideZard(),
+  ],
 };
